354acaf850ea43c1a61d69d8abc3a408aa032977
lewis sneddon <lewis@pop-os.localdomain>
2023-04-30 17:09:36 +0100

    loaded js content

--- src/views/app/items.rs
+++ src/views/app/items.rs
@@ -3,7 +3,9 @@ use actix_web::HttpResponse;
 use super::content_loader::read_file;
 
 pub async fn items() -> HttpResponse {
-    let content = read_file("templates/main.html");
+    let mut content = read_file("templates/main.html");
+    let js_content = read_file("templates/main.js");
+    content = content.replace("{{JAVASCRIPT}}", &js_content);
     HttpResponse::Ok()
         .content_type("text/html;charset=utf-8")
         .body(content)
