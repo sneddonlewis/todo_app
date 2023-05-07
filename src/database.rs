758104141b7e9f4b9d92d7c0face483cf976a74c
lewis sneddon <lewis@pop-os.localdomain>
2023-05-07 16:03:53 +0100

    Pooled connection manager

--- src/database.rs
+++ src/database.rs
@@ -1,19 +1,53 @@
+use actix_web::dev::Payload;
+use actix_web::error::ErrorServiceUnavailable;
+use actix_web::{Error, FromRequest, HttpRequest};
+use futures::future::{Ready, ok, err};
+use lazy_static::lazy_static;
 use diesel::prelude::*;
-use diesel::pg::PgConnection;
+use diesel::{pg::PgConnection, r2d2::{Pool, ConnectionManager, PooledConnection}};
 use crate::config::Config;
 use dotenv::dotenv;
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
+type PgPool = Pool<ConnectionManager<PgConnection>>;
 
-    let config = Config::new();
-    let database_url = config.map
-        .get("DB_URL")
-        .unwrap()
-        .as_str()
-        .unwrap()
-        .to_string();
+pub struct DbConnection {
+    pub db_connection: PgPool,
+}
 
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+lazy_static! {
+    pub static ref DBCONNECTION: DbConnection = {
+        let connection_string = Config::new()
+            .map
+            .get("DB_URL")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .to_string();
+        DbConnection {
+            db_connection: PgPool::builder()
+                .max_size(8)
+                .build(ConnectionManager::new(connection_string))
+                .expect("failed to create db connection pool")
+        }
+    };
+}
+
+pub fn establish_connection() -> PooledConnection<ConnectionManager<PgConnection>> {
+    DBCONNECTION.db_connection.get().unwrap()
+}
+
+pub struct DB {
+    pub connection: PooledConnection<ConnectionManager<PgConnection>>,
+}
+
+impl FromRequest for DB {
+    type Error = Error;
+    type Future = Ready<Result<DB, Error>>;
+
+    fn from_request(_: &HttpRequest, _: &mut Payload) -> Self::Future {
+        match DBCONNECTION.db_connection.get() {
+            Ok(connection) => ok(DB{connection}),
+            Err(_) => err(ErrorServiceUnavailable("could not make connection to database")),
+        }
+    }
 }
--- src/views/to_do/edit.rs
+++ src/views/to_do/edit.rs
@@ -1,21 +1,19 @@
-use crate::{diesel, database::establish_connection};
+use crate::{diesel, database::DB};
 use diesel::prelude::*;
 use actix_web::{HttpResponse, web::Json};
 
 use crate::{json_serialization::{to_do_item::ToDoItem, to_do_items::ToDoItems}, jwt::JwToken};
 use crate::schema::to_do;
 
-pub async fn edit(to_do_item: Json<ToDoItem>, token: JwToken) -> HttpResponse {
+pub async fn edit(to_do_item: Json<ToDoItem>, token: JwToken, db: DB) -> HttpResponse {
     println!("JWT message: {}", token.message);
-
-    let connection = establish_connection();
 
     let results = to_do::table
         .filter(to_do::columns::title.eq(&to_do_item.title));
 
     let _ = diesel::update(results)
         .set(to_do::columns::status.eq("DONE"))
-        .execute(&connection);
+        .execute(&db.connection);
 
     HttpResponse::Ok().json(ToDoItems::get_state())
 }
